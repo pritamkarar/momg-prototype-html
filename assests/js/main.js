0f346d602ca3378dc50a39fac64b4707bcb10bd4
Vicky <107836055+chandan2012@users.noreply.github.com>
2025-01-02 12:44:08 +0530

    music nft done

--- assests/js/main.js
+++ assests/js/main.js
@@ -48,13 +48,65 @@ new WOW().init();
       dots: false,
     });
   }
+  function dropdown(e) {
+    var obj = $(e + ".dropdown");
+    var btn = obj.find(".btn-selector");
+    var dd = obj.find("ul");
+    var opt = dd.find("li");
 
+    obj
+      .on("mouseenter", function () {
+        dd.show();
+        $(this).css("z-index", 1000);
+      })
+      .on("mouseleave", function () {
+        dd.hide();
+        $(this).css("z-index", "auto");
+      });
+
+    opt.on("click", function () {
+      dd.hide();
+      var txt = $(this).text();
+      opt.removeClass("active");
+      $(this).addClass("active");
+      btn.text(txt);
+    });
+  }
   jQuery(document).ready(function () {
     var $doc_height = jQuery(window).innerHeight();
 
     $(".full-height").css("min-height", $doc_height);
 
+    jQuery(".nft__item_click").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+
+      switch (iteration) {
+        case 1:
+          var cover = jQuery(this).parent().parent().find(".nft__item_extra");
+          cover.css("visibility", "visible");
+          cover.css("opacity", "1");
+          break;
+        case 2:
+          var cover = jQuery(this).parent().parent().find(".nft__item_extra");
+          cover.css("visibility", "hidden");
+          cover.css("opacity", "0");
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    $(".nft__item").mouseleave(function () {
+      var cover = jQuery(this).find(".nft__item_extra");
+      cover.css("visibility", "hidden");
+      cover.css("opacity", "0");
+      jQuery(this).find(".nft__item_click").data("iteration", 1);
+    });
     centerY();
     load_owl();
+    dropdown("#item_category");
+    dropdown("#buy_category");
+    dropdown("#items_type");
   });
 })(jQuery);
