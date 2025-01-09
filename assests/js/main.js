cfebd8422d617020972de907037fbd622c78a730
Chandanshaw004 <107836055+chandan2012@users.noreply.github.com>
2025-01-09 16:55:48 +0530

    Onek kaj

--- assests/js/main.js
+++ assests/js/main.js
@@ -101,179 +101,313 @@ new WOW().init();
 
   function menu_arrow() {
     // mainmenu create span
-    jQuery('#mainmenu li a').each(function() {
-        if ($(this).next("ul").length > 0) {
-            $("<span></span>").insertAfter($(this));
-        }
+    jQuery("#mainmenu li a").each(function () {
+      if ($(this).next("ul").length > 0) {
+        $("<span></span>").insertAfter($(this));
+      }
     });
     // mainmenu arrow click
-    jQuery("#mainmenu > li > span").on("click", function() {
-        
-        var iteration = $(this).data('iteration') || 1;
-        switch (iteration) {
-            case 1:
-                $(this).addClass("active");
-                $(this).parent().find("ul:first").css("height", "auto");
-                var curHeight = $(this).parent().find("ul:first").height();
-                $(this).parent().find("ul:first").css("height", "0");
-                $(this).parent().find("ul:first").animate({
-                    'height': curHeight
-                }, 300, 'easeOutQuint');
-      $('header').css("height", $('#mainmenu')[0].scrollHeight+curHeight+(parseInt($tmp_h)*2));
-                break;
-            case 2:
-     var curHeight = $(this).parent().find("ul:first").height();
-                $(this).removeClass("active");
-                $(this).parent().find("ul:first").animate({
-                    'height': "0"
-                }, 300, 'easeOutQuint');
-      $('header').css("height", $('#mainmenu')[0].scrollHeight-curHeight+(parseInt($tmp_h)*2));
-                break;
+    jQuery("#mainmenu > li > span").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+      switch (iteration) {
+        case 1:
+          $(this).addClass("active");
+          $(this).parent().find("ul:first").css("height", "auto");
+          var curHeight = $(this).parent().find("ul:first").height();
+          $(this).parent().find("ul:first").css("height", "0");
+          $(this).parent().find("ul:first").animate(
+            {
+              height: curHeight,
+            },
+            300,
+            "easeOutQuint"
+          );
+          $("header").css(
+            "height",
+            $("#mainmenu")[0].scrollHeight + curHeight + parseInt($tmp_h) * 2
+          );
+          break;
+        case 2:
+          var curHeight = $(this).parent().find("ul:first").height();
+          $(this).removeClass("active");
+          $(this).parent().find("ul:first").animate(
+            {
+              height: "0",
+            },
+            300,
+            "easeOutQuint"
+          );
+          $("header").css(
+            "height",
+            $("#mainmenu")[0].scrollHeight - curHeight + parseInt($tmp_h) * 2
+          );
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+    jQuery("#mainmenu > li > ul > li > span").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+      switch (iteration) {
+        case 1:
+          $(this).addClass("active");
+          $(this).parent().find("ul:first").css("height", "auto");
+          $(this)
+            .parent()
+            .parent()
+            .parent()
+            .find("ul:first")
+            .css("height", "auto");
+          var curHeight = $(this).parent().find("ul:first").height();
+          $(this).parent().find("ul:first").css("height", "0");
+          $(this).parent().find("ul:first").animate(
+            {
+              height: curHeight,
+            },
+            400,
+            "easeInOutQuint"
+          );
+          break;
+        case 2:
+          $(this).removeClass("active");
+          $(this).parent().find("ul:first").animate(
+            {
+              height: "0",
+            },
+            400,
+            "easeInOutQuint"
+          );
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    jQuery(".nft__item_click").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+
+      switch (iteration) {
+        case 1:
+          var cover = jQuery(this).parent().parent().find(".nft__item_extra");
+          cover.css("visibility", "visible");
+          cover.css("opacity", "1");
+          break;
+        case 2:
+          var cover = jQuery(this).parent().parent().find(".nft__item_extra");
+          cover.css("visibility", "hidden");
+          cover.css("opacity", "0");
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    $(".nft__item").mouseleave(function () {
+      var cover = jQuery(this).find(".nft__item_extra");
+      cover.css("visibility", "hidden");
+      cover.css("opacity", "0");
+      jQuery(this).find(".nft__item_click").data("iteration", 1);
+    });
+
+    jQuery(".nft__item_like").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+
+      switch (iteration) {
+        case 1:
+          $(this).find("i").addClass("active");
+          var val = parseInt($(this).find("span").text()) + 1;
+          $(this).find("span").text(val);
+          break;
+        case 2:
+          $(this).find("i").removeClass("active");
+          var val = parseInt($(this).find("span").text()) - 1;
+          $(this).find("span").text(val);
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    jQuery(".play-pause").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+      var track = $(this).parent().parent().find(".track");
+      var circle = $(this).parent().parent().parent().find(".circle-ripple");
+
+      switch (iteration) {
+        case 1:
+          track[0].play();
+          $(this).addClass("pause");
+          $(this).removeClass("play");
+          circle.fadeIn();
+          break;
+        case 2:
+          track[0].pause();
+          $(this).addClass("play");
+          $(this).removeClass("pause");
+          circle.fadeOut();
+          break;
+      }
+
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    jQuery("#de-click-menu-profile").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+
+      switch (iteration) {
+        case 1:
+          $("#de-submenu-profile").show();
+          $("#de-submenu-profile").addClass("open");
+          $("#de-submenu-notification").removeClass("open");
+          $("#de-submenu-notification").hide();
+          $("#de-click-menu-notification").data("iteration", 1);
+          break;
+        case 2:
+          $("#de-submenu-profile").removeClass("open");
+          $("#de-submenu-profile").hide();
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+
+    jQuery("#de-click-menu-notification").on("click", function () {
+      var iteration = $(this).data("iteration") || 1;
+
+      switch (iteration) {
+        case 1:
+          $("#de-submenu-notification").show();
+          $("#de-submenu-notification").addClass("open");
+          $("#de-submenu-profile").removeClass("open");
+          $("#de-submenu-profile").hide();
+          $("#de-click-menu-profile").data("iteration", 1);
+          break;
+        case 2:
+          $("#de-submenu-notification").removeClass("open");
+          $("#de-submenu-notification").hide();
+          break;
+      }
+      iteration++;
+      if (iteration > 2) iteration = 1;
+      $(this).data("iteration", iteration);
+    });
+  }
+
+  function custom_elements() {
+    // --------------------------------------------------
+    // tabs
+    // --------------------------------------------------
+    jQuery(".de_tab").find(".de_tab_content > div").hide();
+    jQuery(".de_tab").find(".de_tab_content > div:first").show();
+    jQuery("li").find(".v-border").fadeTo(150, 0);
+    jQuery("li.active").find(".v-border").fadeTo(150, 1);
+    jQuery(".de_nav li").on("click", function () {
+      jQuery(this).parent().find("li").removeClass("active");
+      jQuery(this).addClass("active");
+      jQuery(this).parent().parent().find(".v-border").fadeTo(150, 0);
+      jQuery(this).parent().parent().find(".de_tab_content > div").hide();
+      var indexer = jQuery(this).index(); //gets the current index of (this) which is #nav li
+      jQuery(this)
+        .parent()
+        .parent()
+        .find(".de_tab_content > div:eq(" + indexer + ")")
+        .fadeIn(); //uses whatever index the link has to open the corresponding box
+      jQuery(this).find(".v-border").fadeTo(150, 1);
+    });
+    // request quote function
+    var rq_step = 1;
+    jQuery("#request_form .btn-right").on("click", function () {
+      var rq_name = $("#rq_name").val();
+      var rq_email = $("#rq_email").val();
+      var rq_phone = $("#rq_phone").val();
+      if (rq_step == 1) {
+        if (rq_name.length == 0) {
+          $("#rq_name").addClass("error_input");
+        } else {
+          $("#rq_name").removeClass("error_input");
         }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
-    });
-    jQuery("#mainmenu > li > ul > li > span").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        switch (iteration) {
-            case 1:
-                $(this).addClass("active");
-                $(this).parent().find("ul:first").css("height", "auto");
-                $(this).parent().parent().parent().find("ul:first").css("height", "auto");
-                var curHeight = $(this).parent().find("ul:first").height();
-                $(this).parent().find("ul:first").css("height", "0");
-                $(this).parent().find("ul:first").animate({
-                    'height': curHeight
-                }, 400, 'easeInOutQuint');
-                break;
-            case 2:
-                $(this).removeClass("active");
-                $(this).parent().find("ul:first").animate({
-                    'height': "0"
-                }, 400, 'easeInOutQuint');
-                break;
+        if (rq_email.length == 0) {
+          $("#rq_email").addClass("error_input");
+        } else {
+          $("#rq_email").removeClass("error_input");
         }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
-    });
-
-    jQuery(".nft__item_click").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        
-        switch (iteration) {
-            case 1:
-                var cover = jQuery(this).parent().parent().find('.nft__item_extra');
-                cover.css("visibility","visible");
-                cover.css("opacity","1");
-                break;
-            case 2:
-                var cover = jQuery(this).parent().parent().find('.nft__item_extra');
-                cover.css("visibility","hidden");
-                cover.css("opacity","0");
-                break;
+        if (rq_phone.length == 0) {
+          $("#rq_phone").addClass("error_input");
+        } else {
+          $("#rq_phone").removeClass("error_input");
         }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
+      }
+      if (rq_name.length != 0 && rq_email.length != 0 && rq_phone.length != 0) {
+        jQuery("#rq_step_1").hide();
+        jQuery("#rq_step_2").fadeIn();
+      }
     });
-
-    $( ".nft__item" ).mouseleave(function() {
-       var cover = jQuery(this).find('.nft__item_extra');
-       cover.css("visibility","hidden");
-       cover.css("opacity","0");
-       jQuery(this).find('.nft__item_click').data('iteration', 1);
+    // --------------------------------------------------
+    // tabs
+    // --------------------------------------------------
+    jQuery(".de_review").find(".de_tab_content > div").hide();
+    jQuery(".de_review").find(".de_tab_content > div:first").show();
+    //jQuery('.de_review').find('.de_nav li').fadeTo(150,.5);
+    jQuery(".de_review").find(".de_nav li:first").fadeTo(150, 1);
+    jQuery(".de_nav li").on("click", function () {
+      de_size();
+      jQuery(this).parent().find("li").removeClass("active");
+      //jQuery(this).parent().find('li').fadeTo(150,.5);
+      jQuery(this).addClass("active");
+      jQuery(this).fadeTo(150, 1);
+      jQuery(this).parent().parent().find(".de_tab_content > div").hide();
+      var indexer = jQuery(this).index(); //gets the current index of (this) which is #nav li
+      jQuery(this)
+        .parent()
+        .parent()
+        .find(".de_tab_content > div:eq(" + indexer + ")")
+        .show(); //uses whatever index the link has to open the corresponding box
     });
-
-    jQuery(".nft__item_like").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        
-        switch (iteration) {
-            case 1:
-                $(this).find("i").addClass("active");
-                var val = parseInt($(this).find("span").text())+1;
-                $(this).find("span").text(val);
-                break;
-            case 2:
-                $(this).find("i").removeClass("active");
-                var val = parseInt($(this).find("span").text())-1;
-                $(this).find("span").text(val);                   
-                break;
-        }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
+    // --------------------------------------------------
+    // toggle
+    // --------------------------------------------------
+    jQuery(".toggle-list h2").addClass("acc_active");
+    jQuery(".toggle-list h2").toggle(
+      function () {
+        jQuery(this).addClass("acc_noactive");
+        jQuery(this).next(".ac-content").slideToggle(200);
+      },
+      function () {
+        jQuery(this).removeClass("acc_noactive").addClass("acc_active");
+        jQuery(this).next(".ac-content").slideToggle(200);
+      }
+    );
+    // --------------------------------------------------
+    // toggle
+    // --------------------------------------------------
+    jQuery(".expand-custom .toggle").click(function () {
+      jQuery(this).stop().toggleClass("clicked");
+      jQuery(this)
+        .stop()
+        .parent()
+        .parent()
+        .parent()
+        .find(".details")
+        .slideToggle(500);
     });
+  }
 
-    jQuery(".play-pause").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        var track = $(this).parent().parent().find('.track');
-        var circle = $(this).parent().parent().parent().find('.circle-ripple');
-
-        switch (iteration) {
-            case 1:
-               track[0].play();
-               $(this).addClass('pause');
-               $(this).removeClass('play');
-               circle.fadeIn();
-               break;
-            case 2:
-               track[0].pause();
-               $(this).addClass('play');
-               $(this).removeClass('pause');
-               circle.fadeOut();
-               break;
-        }
-
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
+  function de_countdown() {
+    $(".de_countdown").each(function () {
+      var y = $(this).data("year");
+      var m = $(this).data("month");
+      var d = $(this).data("day");
+      var h = $(this).data("hour");
+      $(this).countdown({ until: new Date(y, m - 1, d, h) });
     });
-
-    jQuery("#de-click-menu-profile").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        
-        switch (iteration) {
-            case 1:
-                $('#de-submenu-profile').show();
-                $('#de-submenu-profile').addClass('open');
-                $('#de-submenu-notification').removeClass('open');
-                $('#de-submenu-notification').hide();
-                $('#de-click-menu-notification').data('iteration', 1);
-                break;
-            case 2:
-                $('#de-submenu-profile').removeClass('open');
-                $('#de-submenu-profile').hide();        
-                break;
-        }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
-    });
-
-
-    jQuery("#de-click-menu-notification").on("click", function() {
-        var iteration = $(this).data('iteration') || 1;
-        
-        switch (iteration) {
-            case 1:
-                $('#de-submenu-notification').show();
-                $('#de-submenu-notification').addClass('open');
-                $('#de-submenu-profile').removeClass('open');
-                $('#de-submenu-profile').hide();
-                $('#de-click-menu-profile').data('iteration', 1);
-                break;
-            case 2:
-                $('#de-submenu-notification').removeClass('open');
-                $('#de-submenu-notification').hide();        
-                break;
-        }
-        iteration++;
-        if (iteration > 2) iteration = 1;
-        $(this).data('iteration', iteration);
-    });
-}
+  }
 
   jQuery(document).ready(function () {
     "use strict";
@@ -312,15 +446,25 @@ new WOW().init();
     cover.css("opacity", "0");
     jQuery(this).find(".nft__item_click").data("iteration", 1);
   });
-  
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+  de_countdown();
+  custom_elements();
   menu_arrow();
   centerY();
   load_owl();
   dropdown("#item_category");
   dropdown("#buy_category");
   dropdown("#items_type");
-  $('#mainmenu > li:has(ul)').addClass('menu-item-has-children');
-  $('#mainmenu li:has(ul)').addClass('has-child');
+  dropdown("#select_scheme");
+  dropdown("#select_lang");
+  dropdown("#item_collection");
+  $("#mainmenu > li:has(ul)").addClass("menu-item-has-children");
+  $("#mainmenu li:has(ul)").addClass("has-child");
 })(jQuery);
 
 (function ($) {
